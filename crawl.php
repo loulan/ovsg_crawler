fdf8ab7124cb215cd2d5e2b8cfa1067aca455be6
Loulan <loulan@users.noreply.github.com>
2021-11-15 00:34:48 +0100

    Add workarounds for dead links + fix/improve the script

--- crawl.php
+++ crawl.php
@@ -1,6 +1,40 @@
 #!/usr/bin/php
 <?php
 
+/*
+ * This script uses http://www.podcast-onvasgener.fr/ to get a list of all
+ * pages. I found out afterwards that I could just have used
+ * https://www.europe1.fr/emissions/On-va-s-gener to
+ * https://www.europe1.fr/emissions/On-va-s-gener/62.
+ *
+ * It would probably have been more reliable.
+ */
+
+$root_url = "http://www.podcast-onvasgener.fr/";
+$ignored_day_hrefs = array(
+"http://www.europe1.fr/MediaCenter/Emissions/On-va-s-gener/Sons/On-va-s-gener-04-10-12-1263063",
+"http://www.europe1.fr/MediaCenter/Emissions/On-va-s-gener/Sons/On-va-s-gener-06-10-09-39676/",
+"http://www.europe1.fr/MediaCenter/Emissions/On-va-s-gener/Sons/On-va-s-gener-13-10-09-39686/"
+);
+$fixed_day_hrefs = array(
+"http://www.europe1.fr/MediaCenter/Emissions/On-va-s-gener/Sons/On-va-s-gener-17-04-14-2096143"
+=> "https://www.europe1.fr/emissions/On-va-s-gener/On-va-s-gener-17-04-2014-153956",
+"http://www.europe1.fr/MediaCenter/Emissions/On-va-s-gener/Sons/On-va-s-gener-BEST-OF-2109293"
+=> "https://www.europe1.fr/emissions/On-va-s-gener/On-va-s-gener-Best-Of-01-05-14-153482",
+"http://www.europe1.fr/MediaCenter/Emissions/On-va-s-gener/Sons/On-va-s-gener-08-05-13-1510341"
+=> "https://www.europe1.fr/emissions/On-va-s-gener/On-va-s-gener-Best-of-08-05-13-120950",
+"http://www.europe1.fr/MediaCenter/Emissions/On-va-s-gener/Sons/On-va-s-gener-30-09-13-1658445"
+=> "https://www.europe1.fr/emissions/On-va-s-gener/On-va-s-gener-30-09-13-134518",
+"http://www.europe1.fr/MediaCenter/Emissions/On-va-s-gener/Sons/On-va-s-gener-01-10-13-1659817"
+=> "https://www.europe1.fr/emissions/On-va-s-gener/On-va-s-gener-01-10-13-139008",
+"http://www.europe1.fr/MediaCenter/Emissions/On-va-s-gener/Sons/On-va-s-gener-02-10-13-1661163"
+=> "https://www.europe1.fr/emissions/On-va-s-gener/On-va-s-gener-02-10-13-134528",
+"" // Very hacky, but works, because there's a single empty link...
+=> "https://www.europe1.fr/emissions/On-va-s-gener/On-va-s-gener-30-11-12-105142",
+"hhttp://www.europe1.fr/MediaCenter/Emissions/On-va-s-gener/Sons/On-va-s-gener-29-03-10-165957/"
+=> "http://www.europe1.fr/MediaCenter/Emissions/On-va-s-gener/Sons/On-va-s-gener-29-03-10-165957/"
+);
+
 $sources = array("www_europe1_fr");
 
 $month_str_to_num = array("Janvier" => "01", 
@@ -20,12 +54,37 @@ $day = "XX";
 $month = "XX";
 $year = "XX";
 
+/******************************************************************************/
+
 function parse_www_europe1_fr($page_url)
 {
   global $day, $month, $year;
 
+  $prefix = $year . $month . $day . "_";
+
+  echo "Page: " . $page_url . "\n";
+
+  $file_contents = @file_get_contents($page_url);
+  if ($file_contents == false)
+  {
+    fwrite(STDERR, "\033[01;33mWARNING: couldn't download the page " .
+                   "['$page_url']\033[0m\n");
+    return;
+  }
+  else
+  {
+    file_put_contents("output/www_europe1_fr/pages/" . $prefix
+                      . basename($page_url), $file_contents);
+  }
+
   $dom = new DOMDocument("1.0");
   @$dom->loadHTMLFile($page_url);
+  if ($dom == false)
+  {
+    fwrite(STDERR, "\033[01;33mWARNING: couldn't load the HTML file " .
+                   "['$page_url']\033[0m\n");
+    return;
+  }
 
   $scripts = $dom->getElementsByTagName('script');
   foreach ($scripts as $script)
@@ -45,39 +104,40 @@ function parse_www_europe1_fr($page_url)
     $mp3_url = $script_json["contentUrl"];
   }
 
-  echo "Page: " . $page_url . "\n";
-
   if (!isset($mp3_url))
   {
     fwrite(STDERR, "\033[01;33mWARNING: couldn't find the URL of the " .
-                   "mp3!\033[0m\n");
+                   "mp3\033[0m\n");
     return;
   }
 
   echo "mp3: " . $mp3_url . "\n";
 
-  if (!file_exists("output/www_europe1_fr/pages"))
+  $file_contents = @file_get_contents($mp3_url);
+  if ($file_contents == false)
   {
-    mkdir("output/www_europe1_fr/pages/", 0755, true);
+    fwrite(STDERR, "\033[01;33mWARNING: couldn't download the mp3 " .
+                   "['$mp3_url']\033[0m\n");
+    touch("output/www_europe1_fr/mp3s/" . $prefix . "unavailable");
+    return;
   }
-
-  if (!file_exists("output/www_europe1_fr/mp3s"))
+  else
   {
-    mkdir("output/www_europe1_fr/mp3s/", 0755, true);
+    file_put_contents("output/www_europe1_fr/mp3s/" . $prefix
+                      . basename($mp3_url), $file_contents);
   }
-
-  $prefix = $year . $month . $day . "_";
-
-  file_put_contents("output/www_europe1_fr/pages/" . $prefix
-                    . basename($page_url), file_get_contents($page_url));
-  file_put_contents("output/www_europe1_fr/mp3s/" . $prefix
-                    . basename($mp3_url), file_get_contents($mp3_url));
 }
 
 function parse($base_url, $href, $regexp, $function)
 {
   $dom = new DOMDocument("1.0");
   @$dom->loadHTMLFile($base_url . $href);
+  if ($dom == false)
+  {
+    fwrite(STDERR, "\033[01;33mWARNING: couldn't load the HTML file "
+                   . "['$href']\033[0m\n");
+    return;
+  }
 
   $anchors = $dom->getElementsByTagName("a");
   foreach ($anchors as $element)
@@ -88,8 +148,8 @@ function parse($base_url, $href, $regexp, $function)
     {
       if (($num_matches = count($matches)) > 2)
       {
-       fwrite(STDERR, "\033[01;33mWARNING: multiple matches for '$regexp' in " . 
-                      "'$text'!\033[0m\n");
+        fwrite(STDERR, "\033[01;33mWARNING: multiple REGEXP matches " .
+               "['$regexp', '$text']\033[0m\n");
       }
 
       $function($base_url, $href, $matches[1]);
@@ -101,10 +161,25 @@ function parse($base_url, $href, $regexp, $function)
 
 function parse_day($base_url, $href, $match)
 {
-  global $sources, $day;
+  global $ignored_day_hrefs, $fixed_day_hrefs, $sources, $day;
+
   $day = $match;
 
-  if(preg_match("/www.europe1.fr/i", $href))
+  if (in_array($href, $ignored_day_hrefs))
+  {
+    fwrite(STDERR, "\033[01;32mNOTICE: ignoring URL ['$href']\033[0m\n");
+    return;
+  }
+  else if (array_key_exists($href, $fixed_day_hrefs))
+  {
+    $new_href = $fixed_day_hrefs[$href];
+    fwrite(STDERR, "\033[01;32mNOTICE: replacing page URL ['$href', " .
+                   "'$new_href']\033[0m\n");
+
+    $href = $new_href;
+  }
+
+  if (preg_match("/www.europe1.fr/i", $href))
   {
     if (!in_array("www_europe1_fr", $sources))
     {
@@ -115,13 +190,15 @@ function parse_day($base_url, $href, $match)
   }
   else
   {
-    fwrite(STDERR, "\033[01;33mWARNING: source not supported [$href]\033[0m\n");
+    fwrite(STDERR, "\033[01;33mWARNING: source not supported " .
+                   "['$href']\033[0m\n");
   }
 }
 
 function parse_month($base_url, $href, $match)
 {
   global $month_str_to_num, $month;
+
   $month = $month_str_to_num[$match];
 
   parse($base_url, $href, "/([0-3][0-9]) [0-1][0-9] 20[0-9][0-9]/i",
@@ -131,6 +208,7 @@ function parse_month($base_url, $href, $match)
 function parse_year($base_url, $href, $match)
 {
   global $year;
+
   $year = $match;
 
   parse($base_url, $href,
@@ -144,6 +222,18 @@ function parse_root($base_url, $href)
   parse($base_url, $href, "/20([0-9][0-9])/i", "parse_year");
 }
 
-parse_root("http://www.podcast-onvasgener.fr/", "");
+/******************************************************************************/
+
+if (!file_exists("output/www_europe1_fr/pages"))
+{
+  mkdir("output/www_europe1_fr/pages/", 0755, true);
+}
+
+if (!file_exists("output/www_europe1_fr/mp3s"))
+{
+  mkdir("output/www_europe1_fr/mp3s/", 0755, true);
+}
+
+parse_root($root_url, "");
 
 ?>
